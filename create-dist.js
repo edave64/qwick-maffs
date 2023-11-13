ae2dcfde2a0d5037e7bdf0262a2e3abe3c744b50
edave64 <github5815@xcvbnm.org>
2023-11-13 20:27:19 +0100

    Fix prettier

--- create-dist.js
+++ create-dist.js
@@ -1,18 +1,38 @@
 const fs = require('fs');
 const src = fs.readFileSync('./src/index.js', { encoding: 'utf8' });
 
-fs.writeFile('./dist/qwick-maffs.global.js', `(function () {
+fs.writeFile(
+	'./dist/qwick-maffs.global.js',
+	`(function () {
 ${src}
 (typeof globalThis === 'undefined' ? window : globalThis).QwickMaffs = QwickMaffs;
-})()`, { encoding: 'utf8' }, () => {});
+})()`,
+	{ encoding: 'utf8' },
+	() => {}
+);
 
-fs.writeFile('./dist/qwick-maffs.amd.js', `define([], function () {
+fs.writeFile(
+	'./dist/qwick-maffs.amd.js',
+	`define([], function () {
 ${src}
 return QwickMaffs;
-})`, { encoding: 'utf8' }, () => {});
+})`,
+	{ encoding: 'utf8' },
+	() => {}
+);
 
-fs.writeFile('./dist/qwick-maffs.cjs', `${src}
-module.exports = QwickMaffs;`, { encoding: 'utf8' }, () => {});
+fs.writeFile(
+	'./dist/qwick-maffs.cjs',
+	`${src}
+module.exports = QwickMaffs;`,
+	{ encoding: 'utf8' },
+	() => {}
+);
 
-fs.writeFile('./dist/qwick-maffs.mjs', `${src}
-export default QwickMaffs;`, { encoding: 'utf8' }, () => {});
+fs.writeFile(
+	'./dist/qwick-maffs.mjs',
+	`${src}
+export default QwickMaffs;`,
+	{ encoding: 'utf8' },
+	() => {}
+);
--- tests/maffs.test.js
+++ tests/maffs.test.js
@@ -6,9 +6,9 @@ var QwickMaffs = require('../dist/qwick-maffs.cjs');
  * @param {Object} [opts]
  */
 function simpleTest(input, output, opts) {
-    test('"' + input + '" -> ' + output, function () {
-        expect(QwickMaffs.exec(input, opts)).toBe(output);
-    })
+	test('"' + input + '" -> ' + output, function () {
+		expect(QwickMaffs.exec(input, opts)).toBe(output);
+	});
 }
 
 /**
@@ -17,65 +17,69 @@ function simpleTest(input, output, opts) {
  * @param {Object} [opts]
  */
 function errorTest(input, error, opts) {
-    test('"' + input + '"', function () {
-        expect(QwickMaffs.exec(input, opts)).toStrictEqual(error);
-    })
+	test('"' + input + '"', function () {
+		expect(QwickMaffs.exec(input, opts)).toStrictEqual(error);
+	});
 }
 
 describe('Basics', function () {
-    simpleTest("1", 1);
-    simpleTest("42", 42);
-    simpleTest("1.0", 1);
-    simpleTest("1.1", 1.1);
-    simpleTest("1c1", 1.1, { decimalSep: 'c' });
-    simpleTest("1c1", 1.1, { decimalSep: /\w/ });
-    simpleTest(".1", 0.1);
-    simpleTest("1.1e1", 11);
-    simpleTest("3 + 3", 6);
-    simpleTest("3 - 3", 0);
-    simpleTest("3 * 3", 9);
-    simpleTest("3 / 3", 1);
-    simpleTest("3 ^ 3", 27);
-    simpleTest("3²", 9);
-    simpleTest("3³", 27);
-    simpleTest("(2) + 1", 3);
-})
+	simpleTest('1', 1);
+	simpleTest('42', 42);
+	simpleTest('1.0', 1);
+	simpleTest('1.1', 1.1);
+	simpleTest('1c1', 1.1, { decimalSep: 'c' });
+	simpleTest('1c1', 1.1, { decimalSep: /\w/ });
+	simpleTest('.1', 0.1);
+	simpleTest('1.1e1', 11);
+	simpleTest('3 + 3', 6);
+	simpleTest('3 - 3', 0);
+	simpleTest('3 * 3', 9);
+	simpleTest('3 / 3', 1);
+	simpleTest('3 ^ 3', 27);
+	simpleTest('3²', 9);
+	simpleTest('3³', 27);
+	simpleTest('(2) + 1', 3);
+});
 
 describe('Order of operations', function () {
-    simpleTest("1 + 2 * 4 ^ 5", 2049);
-    simpleTest("(1 + 2) * 4 ^ 5", 3072);
-    simpleTest("(1 + 2 * 4) ^ 5", 59049);
-    simpleTest("((1 + 2) * 4) ^ 5", 248832);
-})
+	simpleTest('1 + 2 * 4 ^ 5', 2049);
+	simpleTest('(1 + 2) * 4 ^ 5', 3072);
+	simpleTest('(1 + 2 * 4) ^ 5', 59049);
+	simpleTest('((1 + 2) * 4) ^ 5', 248832);
+});
 
 describe('Errors', function () {
-    errorTest('1 + (4', {
-        error: QwickMaffs.Error.UnbalancedParentesis,
-        pos: 6
-    });
+	errorTest('1 + (4', {
+		error: QwickMaffs.Error.UnbalancedParentesis,
+		pos: 6,
+	});
 
-    errorTest('1) + 4', {
-        error: QwickMaffs.Error.UnbalancedParentesis,
-        pos: 1
-    });
+	errorTest('1) + 4', {
+		error: QwickMaffs.Error.UnbalancedParentesis,
+		pos: 1,
+	});
 
-    errorTest('wat?', {
-        error: QwickMaffs.Error.UnexpectedSymbol,
-        pos: 0
-    });
+	errorTest('wat?', {
+		error: QwickMaffs.Error.UnexpectedSymbol,
+		pos: 0,
+	});
 
-    errorTest('wat?', {
-        error: QwickMaffs.Error.UnexpectedSymbol,
-        pos: 0
-    });
+	errorTest('wat?', {
+		error: QwickMaffs.Error.UnexpectedSymbol,
+		pos: 0,
+	});
 
-    errorTest('45.+', {
-        error: QwickMaffs.Error.UnexpectedSymbol,
-        pos: 3
-    });
+	errorTest('45.+', {
+		error: QwickMaffs.Error.UnexpectedSymbol,
+		pos: 3,
+	});
 
-    errorTest('45e1', {
-        error: QwickMaffs.Error.UnexpectedSymbol,
-        pos: 2
-    }, { supportENotation: false });
-})
+	errorTest(
+		'45e1',
+		{
+			error: QwickMaffs.Error.UnexpectedSymbol,
+			pos: 2,
+		},
+		{ supportENotation: false }
+	);
+});
