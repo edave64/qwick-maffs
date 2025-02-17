e6bc4471343d9774c849271c0e6f995680b62a2d
edave64 <github5815@xcvbnm.org>
2025-02-17 22:31:10 +0100

    feat: add physical sizes

---
+++ src/units/physicalSize.ts
@@ -0,0 +1,18 @@
+export default {
+	m: {
+		si: true,
+		from: {
+			ft: 0.3048,
+		},
+	},
+	ft: {
+		from: {
+			m: 1 / 0.3048,
+		},
+		alias: {
+			yd: 3,
+			mi: 5280,
+			in: 1 / 12,
+		},
+	},
+};
